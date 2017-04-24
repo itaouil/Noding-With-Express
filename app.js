8419b47fbead630fc5532022b8e26e41fc0e75d1
Ilyass Taouil <itaouil95@gmail.com>
2017-04-24 21:02:13 +0200

    Added some really awful jade templating. I am sorry.

--- app.js
+++ app.js
@@ -7,9 +7,24 @@ var bodyParser = require("body-parser");
 // Express object and settings
 var app = express();
 
+// Set view and view engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
 // Set middleware parsers
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({encoded: false}));
+app.use(express.static(path.join(__dirname, "public")));
+
+// Homepage
+app.get("/", function(req, res) {
+  res.render("index");
+});
+
+// About page
+app.get("/about", function(req, res) {
+  res.render("about");
+});
 
 // Start server
 app.listen(3000);
